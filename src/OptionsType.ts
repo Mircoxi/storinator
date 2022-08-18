64b2f911218a3b99fe338fa5379aef39f5db2edd
Mircoxi <mircoxi@protonmail.com>
2022-08-18 18:04:16 +0100

    Added Options type template for initial functionality

---
+++ src/OptionsType.ts
@@ -0,0 +1,13 @@
+import {EncodingTypeEnum} from "./EncodingTypeEnum";
+
+export type OptionsType = {
+    /**
+     * Used by the API to determine whether to set an expiry time.
+     */
+    expireIn?: number
+    /**
+     * Optionally, a parameter can be set to disallow overwriting the
+     * storage object. Should default to false.
+     */
+    disallowOverwrite?: boolean
+}
